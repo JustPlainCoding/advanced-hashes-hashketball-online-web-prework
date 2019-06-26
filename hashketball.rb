19bd0d79735d4f41a509c1a990629187f618965a
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-26 21:08:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -24,7 +24,7 @@ def game_hash
           assists: 12,
           steals: 12,
           blocks: 12,
-          slam_dunk: 7
+          slam_dunks: 7
         }, 
         "Brook Lopez" => {
           number: 11,
@@ -34,7 +34,7 @@ def game_hash
           assists: 10,
           steals: 3,
           blocks: 1,
-          slam_dunk: 15
+          slam_dunks: 15
         },
         "Mason Plumlee" => {
           number: 1,
@@ -44,7 +44,7 @@ def game_hash
           assists: 6,
           steals: 3,
           blocks: 8,
-          slam_dunk: 5
+          slam_dunks: 5
         },
         "Jason Terry" => {
           number: 31,
@@ -54,7 +54,7 @@ def game_hash
           assists: 2,
           steals: 4,
           blocks: 11,
-          slam_dunk: 1
+          slam_dunks: 1
         }
       ]
     },
@@ -70,7 +70,7 @@ def game_hash
           assists: 1,
           steals: 2,
           blocks: 7,
-          slam_dunk: 2
+          slam_dunks: 2
         },
         "Bismack Biyombo" => {
           number: 0,
@@ -80,7 +80,7 @@ def game_hash
           assists: 7,
           steals: 22,
           blocks: 15,
-          slam_dunk: 10
+          slam_dunks: 10
         }, 
         "DeSagna Diop" => {
           number: 2,
@@ -90,7 +90,7 @@ def game_hash
           assists: 12,
           steals: 4,
           blocks: 5,
-          slam_dunk: 5
+          slam_dunks: 5
         },
         "Ben Gordon" => {
           number: 8,
@@ -100,7 +100,7 @@ def game_hash
           assists: 2,
           steals: 1,
           blocks: 1,
-          slam_dunk: 0
+          slam_dunks: 0
         },
         "Kemba Walker" => {
           number: 33,
@@ -110,7 +110,7 @@ def game_hash
           assists: 12,
           steals: 7,
           blocks: 15,
-          slam_dunk: 12
+          slam_dunks: 12
         }
         ]
     }
